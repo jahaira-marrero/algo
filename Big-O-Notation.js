a199e11c23fffd0e850415b3963e0f67ec6be563
Jahaira <jahaira.marrero@gmail.com>
2021-07-06 10:26:58 -0400

    practice questions

--- Big-O-Notation.js
+++ Big-O-Notation.js
@@ -200,3 +200,58 @@ O(n) operation inside of an O(n) operation is O(n^2).
 So as N grows, the runtime grows even larger, grows and times and larger roughly.
 
 */
+
+// QUIZ --------------------------------------------
+
+
+// Determine the time complexity for the following function :
+
+function logUpTo(n) {
+    for (var i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+// O(n)
+
+function logAtMost10(n) {
+    for (var i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+// O(1)
+
+function logAtLeast10(n) {
+    for (var i = 1; i <= Math.max(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+// O(n)
+
+function onlyElementsAtEvenIndex(array) {
+    var newArray = Array(Math.ceil(array.length / 2));
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray[i / 2] = array[i];
+        }
+    }
+    return newArray;
+}
+
+// O(n)
+
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    return subtotalArray;
+}
+
+// O(n^2)
